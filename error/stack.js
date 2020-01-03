22bea9a606152dceeaae1e8f926551a5c235e5a8
아이웍스인터렉티브 <49019267+iworks-interactive@users.noreply.github.com>
2020-01-03 18:10:24 +0900

    stack
    에러추적 스택 자바스크립트

--- error/stack.js
+++ error/stack.js
@@ -1,4 +1,4 @@
-window.onerror = (eventOrMessage, url, lineNumber, colNumber, error) => {    
+window.onerror = function (eventOrMessage, url, lineNumber, colNumber, error) {
     if (typeof eventOrMessage !== 'string') {
         error = eventOrMessage.error;
         url = eventOrMessage.filename || eventOrMessage.fileName;
@@ -6,19 +6,15 @@ window.onerror = (eventOrMessage, url, lineNumber, colNumber, error) => {
         colNumber = eventOrMessage.colno || eventOrMessage.columnNumber;
         eventOrMessage = eventOrMessage.message || eventOrMessage.name || error.message || error.name;
     }
-    let stack:string = (error && error.stack) ? (()=>error.stack.split("\n").map((x:string)=>{return x.trim()}).join(" "))() : "";
-    let userAgent = window.navigator.userAgent;
-        
-    if (typeof($) === "function" || typeof(jQuery) === "function")
-    {
-        $.get("http://localhost:8000/api/Error/stack",{"e":eventOrMessage,"j":url,"l":lineNumber,"c":colNumber,"s":stack});
+    var stack = (error && error.stack) ? (function () { return error.stack.split("\n").map(function (x) { return x.trim(); }).join(" "); })() : "";
+    var userAgent = window.navigator.userAgent;
+    if (typeof ($) === "function" || typeof (jQuery) === "function") {
+        $.get("http://localhost:8000/api/Error/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
     }
-    else if (typeof(fetch) === "function")
-    {
-        fetch("http://localhost:8000/api/Error/stack",{"e":eventOrMessage,"j":url,"l":lineNumber,"c":colNumber,"s":stack});
+    else if (typeof (fetch) === "function") {
+        fetch("http://localhost:8000/api/Error/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
     }
-    else
-    {        
-        new Image().src = `http://localhost:8000/api/Error/stack?e=${encodeURIComponent(eventOrMessage)}&j=${encodeURIComponent(url)}&l=${encodeURIComponent(lineNumber)}&c=${encodeURIComponent(colNumber)}&s=${encodeURIComponent(stack)}`;
+    else {
+        new Image().src = "http://localhost:8000/api/Error/stack?e=" + encodeURIComponent(eventOrMessage) + "&j=" + encodeURIComponent(url) + "&l=" + encodeURIComponent(lineNumber) + "&c=" + encodeURIComponent(colNumber) + "&s=" + encodeURIComponent(stack);
     }
-}
+};
