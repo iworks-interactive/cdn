5a4de9778c662986ac3c818fee90871299c75b71
아이웍스인터렉티브 <49019267+iworks-interactive@users.noreply.github.com>
2020-01-06 18:40:37 +0900

    error stack 주소 변경

--- error/stacks.js
+++ error/stacks.js
@@ -9,12 +9,12 @@ window.onerror = function (eventOrMessage, url, lineNumber, colNumber, error) {
     var stack = (error && error.stack) ? (function () { return error.stack.split("\n").map(function (x) { return x.trim(); }).join(" "); })() : "";
     var userAgent = window.navigator.userAgent;
     if (typeof ($) === "function" || typeof (jQuery) === "function") {
-        $.get("http://localhost:8000/api/Error/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
+        $.get("http://error.iwi.co.kr/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
     }
     else if (typeof (fetch) === "function") {
-        fetch("http://localhost:8000/api/Error/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
+        fetch("http://error.iwi.co.kr/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
     }
     else {
-        new Image().src = "http://localhost:8000/api/Error/stack?e=" + encodeURIComponent(eventOrMessage) + "&j=" + encodeURIComponent(url) + "&l=" + encodeURIComponent(lineNumber) + "&c=" + encodeURIComponent(colNumber) + "&s=" + encodeURIComponent(stack);
+        new Image().src = "http://error.iwi.co.kr/stack?e=" + encodeURIComponent(eventOrMessage) + "&j=" + encodeURIComponent(url) + "&l=" + encodeURIComponent(lineNumber) + "&c=" + encodeURIComponent(colNumber) + "&s=" + encodeURIComponent(stack);
     }
 };
