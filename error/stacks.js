6a4a4c2fb0713013a0d8cb33df166a549eee6274
아이웍스인터렉티브 <49019267+iworks-interactive@users.noreply.github.com>
2020-01-07 13:21:45 +0900

    Update stacks.js

--- error/stacks.js
+++ error/stacks.js
@@ -1,20 +1,36 @@
-window.onerror = function (eventOrMessage, url, lineNumber, colNumber, error) {
-    if (typeof eventOrMessage !== 'string') {
-        error = eventOrMessage.error;
-        url = eventOrMessage.filename || eventOrMessage.fileName;
-        lineNumber = eventOrMessage.lineno || eventOrMessage.lineNumber;
-        colNumber = eventOrMessage.colno || eventOrMessage.columnNumber;
-        eventOrMessage = eventOrMessage.message || eventOrMessage.name || error.message || error.name;
-    }
-    var stack = (error && error.stack) ? (function () { return error.stack.split("\n").map(function (x) { return x.trim(); }).join(" "); })() : "";
-    var userAgent = window.navigator.userAgent;
-    if (typeof ($) === "function" || typeof (jQuery) === "function") {
-        $.get("http://error.iwi.co.kr/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
-    }
-    else if (typeof (fetch) === "function") {
-        fetch("http://error.iwi.co.kr/stack", { "e": eventOrMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
-    }
-    else {
-        new Image().src = "http://error.iwi.co.kr/stack?e=" + encodeURIComponent(eventOrMessage) + "&j=" + encodeURIComponent(url) + "&l=" + encodeURIComponent(lineNumber) + "&c=" + encodeURIComponent(colNumber) + "&s=" + encodeURIComponent(stack);
-    }
-};
+try {
+    window.onerror = function (eventOrMessage, url, lineNumber, colNumber, error) {
+        try {
+            var everntMessage = "";
+            if (typeof eventOrMessage !== 'string') {
+                everntMessage = eventOrMessage.type + " " + eventOrMessage.target + " " + eventOrMessage.eventPhase + " " + eventOrMessage.composedPath();
+            }
+            else {
+                everntMessage = eventOrMessage;
+            }
+            var stack = (error && error.stack) ? (function () { return error.stack.split("\n").map(function (x) { return x.trim(); }).join(" "); })() : "";
+            var userAgent = window.navigator.userAgent;
+            if (typeof ($) === "function" || typeof (jQuery) === "function") {
+                $.post("//error.iwi.co.kr/api/Error/stack", { "e": everntMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack });
+            }
+            else if (typeof (fetch) === "function") {
+                fetch("//error.iwi.co.kr/api/Error/stack", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    mode: "cors",
+                    cache: "no-cache",
+                    body: JSON.stringify({ "e": everntMessage, "j": url, "l": lineNumber, "c": colNumber, "s": stack })
+                });
+            }
+            else {
+                new Image().src = "//error.iwi.co.kr/api/Error/stack?e=" + encodeURIComponent(everntMessage) + "&j=" + encodeURIComponent(url) + "&l=" + encodeURIComponent(lineNumber) + "&c=" + encodeURIComponent(colNumber) + "&s=" + encodeURIComponent(stack);
+            }
+        }
+        catch (_a) {
+        }
+    };
+}
+catch (_a) {
+}
